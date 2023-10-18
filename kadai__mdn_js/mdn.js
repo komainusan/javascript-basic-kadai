6cb1586c87adc2735f891ebe3f00ac4fa60f5939
komainusan <144089999+komainusan@users.noreply.github.com>
2023-10-18 16:38:18 +0900

    kadai_mdn_js
    kadai_mdn_jsを追加

---
+++ kadai__mdn_js/mdn.js
@@ -0,0 +1,16 @@
+const getUTCFullYear = () => {
+console.log('2023年');
+}
+
+const getDategetMonth = () => {
+console.log('8月');
+}
+
+const getUTCDate = () => {
+console.log('22日');
+}
+  
+
+getUTCFullYear()
+getDategetMonth()
+getUTCDate()
